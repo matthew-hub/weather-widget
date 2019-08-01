80e80bc6ec472a3f2042c3bd5e636347801984f7
matthew-hub <mateuszr430@gmail.com>
2019-08-01 21:50:08 +0200

    component added  "display city names in dropdown menu"

---
+++ src/components/CityNames.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './CityNames.scss'
+
+const CityNames = (props) => {
+
+  const {cities, cityId, selectCity, click} = props.cityData;
+
+  const cityList = cities.map( city => {
+    return (
+      <li onClick={() => click(city.id, city.name)} key={city.id}>{city.name}</li>
+    )
+  })
+
+  return ( 
+    <div className="weather__city">
+      <div className="city__dropdown">
+        <h3>{cityId === null ? "Select City" : selectCity }</h3>
+        <ul className="city__list">
+          {cityList}
+        </ul>
+      </div>
+    </div>
+  );
+}
+ 
+export default CityNames;
