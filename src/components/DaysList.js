c7490ca3420eed3193bcbdc5c2aaf6050395bfc4
matthew-hub <mateuszr430@gmail.com>
2019-08-01 11:46:01 +0200

    slyling component, added rwd

--- src/components/DaysList.js
+++ src/components/DaysList.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import "./DaysList.scss";
 
-
 const DaysList = (props) => {
-  const gsDayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
   const currentDay = new Date(props.date).getDay();
 
   const days = props.days.map((day, index) => {
+
     let date = new Date(day.date).getDay();
-    
+    let infoTemp = (props.tempType === "fanhrenheit" ? Math.round(day.temperature * 9 / 5 + 32) : day.temperature) ;
+  
     return (
       <div className="weather__days__list" key={index}>
-        <div className="list__day">{date === currentDay ? "Today": gsDayNames[date]}</div>
+        <div className="list__day">{date === currentDay ? "Today": props.daysNames[date]}</div>
         <div className={`list__avatar ${day.type}`}></div>
-        <div>{day.temperature}</div>
-        <div className="list__pollen">{day.pollenCount}</div>
+        <div className="list__temp">{infoTemp} {props.tempType === "fanhrenheit" ? <sup>°F</sup> : <sup>°C</sup>}</div>
+        <div className="list__pollen">{"Pollen " + day.pollenCount} </div>
       </div>
     )
   })
 
   return (  
     <>
-    {days}
+      {days}
     </>
   );
 }
